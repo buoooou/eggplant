c655304111327d88f2357170980a7a9695ebcd9a
zhangkuo <769329489@qq.com>
2019-08-27 21:24:07 +0800

    init commit

--- src/main/java/kafeihu/zk/eggplant/executor/MulThreadPoolExecutor.java
+++ src/main/java/kafeihu/zk/eggplant/executor/MulThreadPoolExecutor.java
@@ -1,7 +1,99 @@
 package kafeihu.zk.eggplant.executor;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class MulThreadPoolExecutor extends AbstractMulExecutorService{
 
 
+    public MulThreadPoolExecutor(int corePoolSize,
+                              int maximumPoolSize,
+                              long keepAliveTime,
+                              TimeUnit unit,
+                              BlockingQueue<Runnable> workQueue) {
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                Executors.defaultThreadFactory(), new AbortHandler());
+    }
 
+    public MulThreadPoolExecutor(int corePoolSize,
+                                 int maximumPoolSize,
+                                 long keepAliveTime,
+                                 TimeUnit unit,
+                                 BlockingQueue<Runnable> workQueue,
+                                 ThreadFactory threadFactory) {
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                threadFactory, new AbortHandler());
+    }
+
+
+    public MulThreadPoolExecutor(int corePoolSize,
+                                 int maximumPoolSize,
+                                 long keepAliveTime,
+                                 TimeUnit unit,
+                                 BlockingQueue<Runnable> workQueue,
+                                 RejectedExecutionHandler handler) {
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                Executors.defaultThreadFactory(), handler);
+    }
+
+    public MulThreadPoolExecutor(int corePoolSize,
+                                 int maximumPoolSize,
+                                 long keepAliveTime,
+                                 TimeUnit unit,
+                                 BlockingQueue<Runnable> workQueue,
+                                 ThreadFactory threadFactory,
+                                 RejectedExecutionHandler handler) {
+        this.executor = new ThreadPoolExecutor(corePoolSize,maximumPoolSize,
+                keepAliveTime,unit,workQueue,threadFactory,handler);
+    }
+
+
+    /**
+     * A handler for rejected tasks that throws a
+     * {@code RejectedExecutionException}.
+     */
+    public static class AbortHandler extends MulExecutorRejectedExecutionHandler {
+        /**
+         * Creates an {@code AbortPolicy}.
+         */
+        public AbortHandler() { }
+
+        /**
+         * Always throws RejectedExecutionException.
+         *
+         * @param worker the runnable task requested to be executed
+         * @param executor the executor attempting to execute this task
+         * @throws RejectedExecutionException always
+         */
+        @Override
+        protected void handleRejectedExecution(Runnable worker, ThreadPoolExecutor executor) {
+            throw new RejectedExecutionException("Task " + worker.toString() +
+                    " rejected from " +
+                    executor.toString());
+        }
+    }
+
+
+
+    @Override
+    public <T> List<Future<T>> submitBegin(Collection<? extends Callable<T>> callables) {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> submitEnd(Collection<? extends Callable<T>> callables) {
+        return null;
+    }
+
+
+    @Override
+    public void executeBegin(Collection<? extends Runnable> runnables) {
+
+    }
+
+    @Override
+    public void executeEnd(Collection<? extends Runnable> runnables) {
+
+    }
 }
